9765479fb07e68af4b37a758f2e64126aa1c1134
Trent Rogers <tarogers479@gmail.com>
2019-05-08 12:57:40 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='IntroAssistant',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/tarogers479/IntroAssistant',
+    install_requires=[
+      'texsoup'
+    ],
+    license='',
+    author='Trent Rogers',
+    author_email='tarogers479@gmail.com',
+    description='Assists in writing introductions.'
+)
